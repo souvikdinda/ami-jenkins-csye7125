a2efc8d9746c458f4198643bcc73b80d3ad7cdf2
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-12-07 00:50:59 -0500

    Updated scripts

--- jenkins/multibranch-pipeline.groovy
+++ jenkins/multibranch-pipeline.groovy
@@ -1,53 +1,52 @@
-multibranchPipelineJob('webapp-build') {
-    displayName('webapp-build')
+pipelineJob('webapp-build') {
+    displayName('WebApp Build')
     description('Creates docker image with release on webapp repository')
-    branchSources {
-        github {
-            id('webapp-build')
-            apiUri('https://api.github.com')
-            repoOwner('csye7125-fall2023-group07')
-            repository('webapp')
-            scanCredentialsId('github_token')
-            includes('*')
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/csye7125-fall2023-group07/webapp.git')
+                        credentials('github_token')
+                    }
+                    branch('*/main')
+                }
+                scriptPath('Jenkinsfile')
+            }
         }
     }
-
-    configure {
-        def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
-    }
-
-    configure { node ->
-     def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
-                spec('')
-                token("WebappJenkinsBuild")
-            }
+    triggers{
+        githubPush()
     }
 }
 
-multibranchPipelineJob('webapp-db-build') {
-    displayName('webapp-db-build')
+pipelineJob('webapp-db-build') {
+    displayName('WebApp DB Build')
     description('Creates docker image with release on webapp-db repository')
-    branchSources {
-        github {
-            id('webapp-db-build')
-            apiUri('https://api.github.com')
-            repoOwner('csye7125-fall2023-group07')
-            repository('webapp-db')
-            scanCredentialsId('github_token')
-            includes('*')
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/csye7125-fall2023-group07/webapp-db.git')
+                        credentials('github_token')
+                    }
+                    branch('*/main')
+                }
+                scriptPath('Jenkinsfile')
+            }
         }
     }
-
-    configure {
-        def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
-    }
-    configure { node ->
-     def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
-                spec('')
-                token("MyWebappDBToken")
-            }
+    triggers{
+        githubPush()
     }
 }
 
@@ -77,55 +76,55 @@ pipelineJob('webapp-helm-chart-build') {
     }
 }
 
-multibranchPipelineJob('producer-app-build') {
-    displayName('producer-app-build')
+pipelineJob('producer-app-build') {
+    displayName('Producer App Build')
     description('Creates docker image with release on producer-app repository')
-    branchSources {
-        github {
-            id('producer-app-build')
-            apiUri('https://api.github.com')
-            repoOwner('csye7125-fall2023-group07')
-            repository('producer-app')
-            scanCredentialsId('github_token')
-            includes('*')
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/csye7125-fall2023-group07/producer-app.git')
+                        credentials('github_token')
+                    }
+                    branch('*/main')
+                }
+                scriptPath('Jenkinsfile')
+            }
         }
     }
-
-    configure {
-        def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
-    }
-    configure { node ->
-     def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
-                spec('')
-                token("ProducerAppBuild")
-            }
+    triggers{
+        githubPush()
     }
 }
 
-multibranchPipelineJob('consumer-app-build') {
-    displayName('consumer-app-build')
+pipelineJob('consumer-app-build') {
+    displayName('Consumer App Build')
     description('Creates docker image with release on consumer-app repository')
-    branchSources {
-        github {
-            id('consumer-app-build')
-            apiUri('https://api.github.com')
-            repoOwner('csye7125-fall2023-group07')
-            repository('consumer-app')
-            scanCredentialsId('github_token')
-            includes('*')
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/csye7125-fall2023-group07/consumer-app.git')
+                        credentials('github_token')
+                    }
+                    branch('*/main')
+                }
+                scriptPath('Jenkinsfile')
+            }
         }
     }
-
-    configure {
-        def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
-    }
-    configure { node ->
-     def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
-                spec('')
-                token("ConsumerAppBuild")
-            }
+    triggers{
+        githubPush()
     }
 }
 
@@ -154,3 +153,110 @@ pipelineJob('infra-helm-chart-build') {
         githubPush()
     }
 }
+
+// Multibranch Pipelines
+
+// multibranchPipelineJob('webapp-build') {
+//     displayName('webapp-build')
+//     description('Creates docker image with release on webapp repository')
+//     branchSources {
+//         github {
+//             id('webapp-build')
+//             apiUri('https://api.github.com')
+//             repoOwner('csye7125-fall2023-group07')
+//             repository('webapp')
+//             scanCredentialsId('github_token')
+//             includes('*')
+//         }
+//     }
+
+//     configure {
+//         def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+//         traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
+//     }
+
+//     configure { node ->
+//      def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+//                 spec('')
+//                 token("WebappJenkinsBuild")
+//             }
+//     }
+// }
+
+// multibranchPipelineJob('webapp-db-build') {
+//     displayName('webapp-db-build')
+//     description('Creates docker image with release on webapp-db repository')
+//     branchSources {
+//         github {
+//             id('webapp-db-build')
+//             apiUri('https://api.github.com')
+//             repoOwner('csye7125-fall2023-group07')
+//             repository('webapp-db')
+//             scanCredentialsId('github_token')
+//             includes('*')
+//         }
+//     }
+
+//     configure {
+//         def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+//         traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
+//     }
+//     configure { node ->
+//      def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+//                 spec('')
+//                 token("MyWebappDBToken")
+//             }
+//     }
+// }
+
+// multibranchPipelineJob('producer-app-build') {
+//     displayName('producer-app-build')
+//     description('Creates docker image with release on producer-app repository')
+//     branchSources {
+//         github {
+//             id('producer-app-build')
+//             apiUri('https://api.github.com')
+//             repoOwner('csye7125-fall2023-group07')
+//             repository('producer-app')
+//             scanCredentialsId('github_token')
+//             includes('*')
+//         }
+//     }
+
+//     configure {
+//         def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+//         traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
+//     }
+//     configure { node ->
+//      def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+//                 spec('')
+//                 token("ProducerAppBuild")
+//             }
+//     }
+// }
+
+// multibranchPipelineJob('consumer-app-build') {
+//     displayName('consumer-app-build')
+//     description('Creates docker image with release on consumer-app repository')
+//     branchSources {
+//         github {
+//             id('consumer-app-build')
+//             apiUri('https://api.github.com')
+//             repoOwner('csye7125-fall2023-group07')
+//             repository('consumer-app')
+//             scanCredentialsId('github_token')
+//             includes('*')
+//         }
+//     }
+
+//     configure {
+//         def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+//         traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
+//     }
+//     configure { node ->
+//      def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+//                 spec('')
+//                 token("ConsumerAppBuild")
+//             }
+//     }
+// }
--- packer/shell-script.sh
+++ packer/shell-script.sh
@@ -103,6 +103,35 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
+# Installing kubectl
+echo "================================="
+echo "Installing kubectl"
+echo "================================="
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# Installing gcloud
+echo "================================="
+echo "Installing gcloud"
+echo "================================="
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt update
+sudo apt install google-cloud-cli
+sudo apt install google-cloud-sdk-gke-gcloud-auth-plugin
+
+# Installing Go & Make
+echo "================================="
+echo "Installing Go & Make"
+echo "================================="
+sudo apt install golang
+sudo apt install make
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+
 # Install caddy
 echo "================================="
 echo "Installing Caddy"
